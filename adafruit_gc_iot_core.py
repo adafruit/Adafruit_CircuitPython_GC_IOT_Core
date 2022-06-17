fd59e85e9b1dc2d9c5682cdf3d13da5e9b55f7d7
JulianOrteil <dakota.horstman2001@gmail.com>
2022-06-17 16:21:18 -0500

    Pylint import order

--- adafruit_gc_iot_core.py
+++ adafruit_gc_iot_core.py
@@ -39,8 +39,8 @@ except ImportError:
     pass
 
 import adafruit_logging as logging
-import rtc
 from adafruit_jwt import JWT
+import rtc
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_GC_IOT_Core.git"
